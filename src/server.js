6dcfd81879a61067693c4e551e49ce34f4386326
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2019-11-03 01:21:48 +0500

    added server file

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const grpc = require('grpc');
+const helloProto = grpc.load('hello.proto');
+const server = new grpc.Server();
+
+
+server.bind('127.0.0.1:50051',
+            grpc.ServerCredentials.createInsecure());
+console.log('server is running at http://127.0.0.1:50051');
+
+server.addService(helloProto.Greeter.service,{
+    sayHello: (call,callback) => {
+        let {name} = call.request;
+        callback(null,{
+            message = "Hello "+name 
+        });
+    }
+});
+server.start()
